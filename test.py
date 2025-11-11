5a605b67e6a0f67316437e57e86a980ca49098ac
DamaineN <damaine334@gmail.com>
2025-11-11 21:02:33 +0800

    removed test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import json, os
-fname = "db/passwords_maine.json"
-print("Exists:", os.path.exists(fname))
-if os.path.exists(fname):
-    with open(fname, "r") as f:
-        print(json.dumps(json.load(f), indent=4))
